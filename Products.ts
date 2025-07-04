0442b6fc714c8e810eefcb40cace5ca4718ce74a
Teki-1 <nournabarawi20@gmail.com>
2025-07-05 00:08:54 +0300

    Add Product interface and initial product instances

---
+++ Products.ts
@@ -0,0 +1,27 @@
+interface Product {
+  name: string;
+  price: number;
+  expirationDate?: Date;
+  weight?: number;
+  shippable?: boolean;
+}
+
+const Product1: Product = {
+  name: "Cheese",
+  price: 5,
+  expirationDate: new Date("2025-7-15"),
+  weight: 2,
+  shippable: true,
+};
+
+const Product2: Product = {
+  name: "TV",
+  price: 2000,
+  weight: 20,
+  shippable: true,
+};
+
+const Product3: Product = {
+  name: "Mobile Scratch Card",
+  price: 50,
+};
