ef576dffa8ddd3a3682c5cc5831dae508b77257b
Teki-1 <nournabarawi20@gmail.com>
2025-07-05 07:05:18 +0300

    Added stock to interface

--- Products.ts
+++ Products.ts
@@ -4,14 +4,16 @@ export interface Product {
   expirationDate?: Date;
   weight?: number;
   shippable?: boolean;
+  stock: boolean;
 }
 
 const Product1: Product = {
   name: "Cheese",
-  price: 5,
+  price: 15,
   expirationDate: new Date("2025-7-15"),
   weight: 250,
   shippable: true,
+  stock: true,
 };
 
 const Product2: Product = {
@@ -19,11 +21,20 @@ const Product2: Product = {
   price: 5000,
   weight: 20,
   shippable: true,
+  stock: false,
 };
 
 const Product3: Product = {
   name: "Mobile Scratch Card",
   price: 50,
+  stock: true,
 };
-
+const Product4: Product = {
+  name: "Banana",
+  price: 5,
+  weight: 120,
+  expirationDate: new Date("2025-7-2"),
+  shippable: true,
+  stock: true,
+};
 export const productslist: Product[] = [Product1, Product2, Product3];
--- addproduct.ts
+++ addproduct.ts
@@ -5,10 +5,12 @@ export class Cart {
     const sum: number = Quantity * product.price;
     const weight = product.weight || 0;
     const shipsum: number = (Quantity * weight) / 1000;
+    if (shipsum > 0) {
+      console.log(Quantity, "x", product.name, "Total weight: ", shipsum, "kg");
+    }
     console.log(Quantity, "x", product.name, "Total price: ", sum);
-    console.log(Quantity, "x", product.name, "Total weight: ", shipsum, "kg");
   }
 }
 
 const cart = new Cart();
-cart.add(productslist[0], 2);
+cart.add(productslist[2], 5);
