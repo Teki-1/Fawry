2c0137c08adf5acead0e85eb8487ca41f37da3c7
Teki-1 <nournabarawi20@gmail.com>
2025-07-05 07:13:35 +0300

    Added error handling to zero quantity and empty cart

--- addproduct.ts
+++ addproduct.ts
@@ -1,7 +1,20 @@
 import { Product, productslist } from "./Products";
 
+type Item = {
+  product: Product;
+  Quantity: number;
+};
+
 export class Cart {
+  private CartItems: Item[] = [];
+
   add(product: Product, Quantity: number): void {
+    if (Quantity <= 0) {
+      console.log("ERROR: Quantity must have value of one or higher !");
+      return;
+    }
+    this.CartItems.push({ product, Quantity });
+
     const sum: number = Quantity * product.price;
     const weight = product.weight || 0;
     const shipsum: number = (Quantity * weight) / 1000;
@@ -9,6 +22,13 @@ export class Cart {
       console.log(Quantity, "x", product.name, "Total weight: ", shipsum, "kg");
     }
     console.log(Quantity, "x", product.name, "Total price: ", sum);
+  }
+
+  EmptyCheckout(): void {
+    if (this.CartItems.length === 0) {
+      console.log("Your cart is empty.");
+      return;
+    }
   }
 }
 
